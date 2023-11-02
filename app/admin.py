dcd26edb083fa38beb7d42f44158a72888b17224
priscahkosgei <jepkogei2020@students.uonbi.ac.ke>
2023-11-01 17:55:20 -0700

    priscahlab

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from app.models import Employee,Product,Teachers
 # Register your models here.
+admin.site.register(Employee)
+admin.site.register(Product)
+admin.site.register(Teachers)
